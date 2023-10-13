2fad14f46ad67923352cc96424167e8d133f373e
Max Claus Nunes <maxcnunes@gmail.com>
2023-10-12 21:28:02 -0300

    Download images concurrently

--- workspace/web-crawler/src/main.rs
+++ workspace/web-crawler/src/main.rs
@@ -10,7 +10,6 @@
 // https://hackernoon.com/parsing-html-with-rust-a-simple-tutorial-using-tokio-reqwest-and-scraper
 use scraper::{Html, Selector};
 
-use std::env;
 use std::{thread, time};
 
 #[derive(Debug)]
@@ -22,87 +21,87 @@ enum DownloadError {
     SaveImageFileFailure,
 }
 
-// TODO: run it async.
-fn download_bird_image(image_page_path: &String) -> Result<(), DownloadError> {
-    let image_page_url = "https://en.wikipedia.org".to_owned() + &image_page_path;
+fn download_bird_image(image_page_path: String) -> thread::JoinHandle<Result<(), DownloadError>> {
+    thread::spawn(move || {
+        let image_page_url = "https://en.wikipedia.org".to_owned() + &image_page_path;
 
-    let client = reqwest::blocking::Client::new();
-    println!("\n\nGetting image page url {}", image_page_url);
+        let client = reqwest::blocking::Client::new();
+        println!("- Getting image page url {}", image_page_url);
 
-    let res = client
-        .get(&image_page_url)
-        .send()
-        .map_err(|_| DownloadError::GetPageFailure)?;
-    println!("Status for {}: {}", image_page_url, res.status());
+        let res = client
+            .get(&image_page_url)
+            .send()
+            .map_err(|_| DownloadError::GetPageFailure)?;
+        // println!("Status for {}: {}", image_page_url, res.status());
 
-    let text = res.text().expect("could not get request");
+        let text = res.text().expect("could not get request");
 
-    let document = Html::parse_document(&text);
-    let img_selector = Selector::parse(".fullImageLink a").expect("could not build parser");
+        let document = Html::parse_document(&text);
+        let img_selector = Selector::parse(".fullImageLink a").expect("could not build parser");
 
-    let link = document
-        .select(&img_selector)
-        .next()
-        .ok_or(DownloadError::GetLinkFailure)?;
+        let link = document
+            .select(&img_selector)
+            .next()
+            .ok_or(DownloadError::GetLinkFailure)?;
 
-    // Wikipedia respond with 403 status code sometimes,
-    // so we need to retry the download for those cases.
-    for attempt in 1..5 {
-        let image_path = link
-            .value()
-            .attr("href")
-            .expect("href attr not foud")
-            .to_string();
+        // Wikipedia respond with 403 status code sometimes,
+        // so we need to retry the download for those cases.
+        for attempt in 1..5 {
+            let image_path = link
+                .value()
+                .attr("href")
+                .expect("href attr not foud")
+                .to_string();
 
-        let image_url = "https:".to_owned() + &image_path;
+            let image_url = "https:".to_owned() + &image_path;
 
-        if attempt > 1 {
-            let sleep_time = time::Duration::from_millis(500);
-            thread::sleep(sleep_time);
+            if attempt > 1 {
+                let sleep_time = time::Duration::from_millis(500);
+                thread::sleep(sleep_time);
+            }
+
+            // println!("Found image url: {}", image_url);
+
+            let file_name = image_page_path.replace("/wiki/File:", "");
+            let file_path = "./static/".to_owned() + &file_name;
+            // println!("Creating file: {}", file_path);
+
+            let mut file = std::fs::File::create(&file_path)
+                .map_err(|_| DownloadError::CreateImageFileFailure)?;
+
+            let mut res_img = reqwest::blocking::get(&image_url)
+                .map_err(|_| DownloadError::DownloadImageFileFailure)?;
+            // println!("Status for {}: {}", image_url, res_img.status());
+
+            if res_img.status().is_success() {
+                res_img
+                    .copy_to(&mut file)
+                    .map_err(|_| DownloadError::SaveImageFileFailure)?;
+                println!("Downloaded image url: {}", image_url);
+                break;
+                // } else if attempt == 5 {
+                //     println!("Failed to download image url: {}", image_url);
+            }
         }
 
-        println!("Found image url: {}", image_url);
-
-        let file_name = image_page_path.replace("/wiki/File:", "");
-        let file_path = "./static/".to_owned() + &file_name;
-        println!("Creating file: {}", file_path);
-
-        let mut file =
-            std::fs::File::create(&file_path).map_err(|_| DownloadError::CreateImageFileFailure)?;
-
-        let mut res_img = reqwest::blocking::get(&image_url)
-            .map_err(|_| DownloadError::DownloadImageFileFailure)?;
-        println!("Status for {}: {}", image_url, res_img.status());
-
-        if res_img.status().is_success() {
-            res_img
-                .copy_to(&mut file)
-                .map_err(|_| DownloadError::SaveImageFileFailure)?;
-            println!("Downloaded image url: {}", image_url);
-            break;
-        } else if attempt == 5 {
-            println!("Failed to download image url: {}", image_url);
-        }
-    }
-
-    Ok(())
+        Ok(())
+    })
 }
 
 fn main() {
-    let path = env::current_dir().unwrap();
-    println!("The current directory is {}", path.display());
-
     let client = reqwest::blocking::Client::new();
     let origin_url = "https://en.wikipedia.org/wiki/Bird";
 
     let res = client.get(origin_url).send().unwrap();
-    println!("Status for {}: {}", origin_url, res.status());
+    // println!("Status for {}: {}", origin_url, res.status());
 
     let text = res.text().expect("could not get request");
 
     let document = Html::parse_document(&text);
     let birds_selector =
         Selector::parse(".clade .clade-leaf .mw-file-description").expect("could not build parser");
+
+    let mut thread_handlers: Vec<thread::JoinHandle<Result<(), DownloadError>>> = Vec::new();
 
     let mut total: i32 = 0;
     for title in document.select(&birds_selector) {
@@ -114,7 +113,9 @@ fn main() {
             .expect("href attr not foud")
             .to_string();
 
-        download_bird_image(&image_page_path).unwrap();
+        let h = download_bird_image(image_page_path.clone());
+
+        thread_handlers.push(h);
 
         if total == 3 {
             // Keep it simple and don't overload Wikipedia with
@@ -123,5 +124,11 @@ fn main() {
         }
     }
 
-    println!("Total: {:?}", total);
+    println!("Downloading {} birds", total);
+
+    for h in thread_handlers {
+        let _ = h.join().unwrap();
+    }
+
+    println!("Completed");
 }

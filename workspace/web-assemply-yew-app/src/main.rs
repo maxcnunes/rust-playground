2cb20be8a00417969ef8f1bfbe49bd50499251ed
Max Claus Nunes <maxcnunes@gmail.com>
2023-10-13 20:50:03 -0300

    Add web assemply project

---
+++ workspace/web-assemply-yew-app/src/main.rs
@@ -0,0 +1,31 @@
+// Web Assembly project using yew framework.
+//
+// To run it:
+//    trunk serve --open
+//
+// Project created based on
+// https://yew.rs/docs/getting-started/build-a-sample-app
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
